7ff33c0e909e98557c58e83e1b7e1d72df798eb4
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2026-01-01 14:34:59 -0500

    remove unused import

--- client/src/pages/Selection.tsx
+++ client/src/pages/Selection.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "wouter";
-import { Button } from "@/components/ui/button";
 import BackToHomeRow from "./selection/sections/BackToHomeRow";
 import BottomTagline from "./selection/sections/BottomTagline";
 import SelectionHero from "./selection/sections/SelectionHero";
